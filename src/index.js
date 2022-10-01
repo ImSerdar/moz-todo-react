ee49744637e7659c1251aee367ff2a55b1f8bd3b
ImSerdar <serdaralkan95@gmail.com>
2022-09-30 23:14:06 -0700

    DATA class, non repetetive task list.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-ReactDOM.render(<App subject="Serdar" />,
+
+const DATA = [
+    { id: "todo-0", name: "Eat", completed: "true"},
+    { id: "todo-1", name: "Sleep", completed: "false"},
+    { id: "todo-2", name: "Repeat", completed: "false"}
+];
+
+ReactDOM.render(<App tasks={DATA} />,
 document.getElementById('root')
 );
